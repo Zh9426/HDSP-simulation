8414cd7db9ef764bbb6c116a4db8864e74836d57
Rommel_Zh89 <2712253798@qq.com>
2026-05-08 22:38:41 +0800

    feat: 调整BIASA弱点补偿参数

--- initial_phase_config.m
+++ initial_phase_config.m
@@ -38,14 +38,14 @@ cfg.min_base_layers = 2;
 
 cfg.phase_case_mode = 'pure_board_iasa';
 cfg.iasa_epochs = 450;
-cfg.iasa_beta = 0.60;
+cfg.iasa_beta = 0.70;
 cfg.iasa_dark_weight = 0.0;
 cfg.iasa_halo_weight = 0.05;
 cfg.iasa_target_gain_limit = 10.0;
 cfg.iasa_anchor_eta = 1.0;
 cfg.iasa_uniformity_enabled = true;
-cfg.iasa_uniformity_beta = 0.45;
-cfg.iasa_uniformity_gain_limit = 1.5;
+cfg.iasa_uniformity_beta = 0.30;
+cfg.iasa_uniformity_gain_limit = 2.5;
 
 cfg.python_epochs = 10000;
 cfg.python_learning_rate = 0.06;
